967ee01d937fe2262dc8a30015ea2ba1f55a5eb1
MaximumPy <m.a.seliwerstov@yandex.ru>
2021-08-15 10:26:04 +0300

    #3 Боту добавлена возможность управления устройствами умного дома путем подачи сигналов на пины GPIO Raspberry Pi

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
+import RPi.GPIO as GPIO
 import telebot
 import key
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.LOW)
+
 bot = telebot.TeleBot(key.token)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+
+    if message.text == "status":
+        if GPIO.output(17) == GPIO.HIGH:
+            bot.send_message(message.from_user.id, "lights are ON")
+        else:
+            bot.send_message(message.from_user.id, "lights are OFF")
+    elif message.text == "enable":
+        bot.send_message(message.from_user.id, "I'll turn it on now")
+        GPIO.output(17, GPIO.HIGH)
+    elif message.text == "disable":
+        bot.send_message(message.from_user.id, "turning it off")
+        GPIO.output(17, GPIO.LOW)
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
+        bot.send_message(message.from_user.id, "Hi")
     else:
-        bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши /help.')
+        bot.send_message(message.from_user.id, 'write /help.')
 bot.polling(none_stop=True, interval=0)
+
+
+if __name__=='__main__':
+    while True:
+        print("key.light")
+        get_text_messages()
+
