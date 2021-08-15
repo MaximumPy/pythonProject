56442974ea6e1d7646d41b061dd1ab65d76d6c8f
MaximumPy <m.a.seliwerstov@yandex.ru>
2021-08-15 15:21:22 +0300

    Fix #3 починил проверку состояния реле

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 import RPi.GPIO as GPIO
 import telebot
-import key
+#import key
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20, GPIO.OUT)
 GPIO.output(20, GPIO.HIGH)
 stat = False
-bot = telebot.TeleBot(key.token)
+bot = telebot.TeleBot("1925884861:AAHHLe1m6lblIaYvjrQUwxiPchFU1g_YZRg")
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
 
-    if message.text == "status":
-        if stat:
-            bot.send_message(message.from_user.id, "lights are OFF")
-        else:
-            bot.send_message(message.from_user.id, "lights are ON")
-    elif message.text == "enable":
-        stat = True
+def get_text_messages(message):
+    if message.text == "enable":
+        print (c.stat)
+        c.stat = True
         bot.send_message(message.from_user.id, "I'll turn it on now")
         GPIO.output(20, GPIO.LOW)
     elif message.text == "disable":
-        stat = False
+        c.stat = False
+
         bot.send_message(message.from_user.id, "turning it off")
         GPIO.output(20, GPIO.HIGH)
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "disable/enable/status")
+    elif message.text == "status":
+        if c.stat == False:
+
+            bot.send_message(message.from_user.id, "lights are OFF")
+        else:
+            print (c.stat)
+            bot.send_message(message.from_user.id, "lights are ON")
     else:
         bot.send_message(message.from_user.id, 'write /help.')
+class stat:
+    def __init__(self):
+        self.stat = False
+        print ("False")
+    def en(self):
+        self.stat = True
+        prinf ("True")
+    def dis(self):
+        self.stat = False
+        prinf ("False")
+c = stat()
 bot.polling(none_stop=True, interval=0)
-print (stat)
