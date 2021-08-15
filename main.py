58134a91dd33e94efb7f019c940f94323838c153
MaximumPy <m.a.seliwerstov@yandex.ru>
2021-08-15 14:31:46 +0300

    Fix #3 мелкие доработки

--- main.py
+++ main.py
@@ -3,28 +3,29 @@ import telebot
 import key
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-GPIO.output(17, GPIO.LOW)
-
+GPIO.setup(20, GPIO.OUT)
+GPIO.output(20, GPIO.HIGH)
+stat = False
 bot = telebot.TeleBot(key.token)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 
     if message.text == "status":
-        if GPIO.output(17) == GPIO.HIGH:
-            bot.send_message(message.from_user.id, "lights are ON")
-        else:
+        if stat:
             bot.send_message(message.from_user.id, "lights are OFF")
+        else:
+            bot.send_message(message.from_user.id, "lights are ON")
     elif message.text == "enable":
+        stat = True
         bot.send_message(message.from_user.id, "I'll turn it on now")
-        GPIO.output(17, GPIO.HIGH)
+        GPIO.output(20, GPIO.LOW)
     elif message.text == "disable":
+        stat = False
         bot.send_message(message.from_user.id, "turning it off")
-        GPIO.output(17, GPIO.LOW)
+        GPIO.output(20, GPIO.HIGH)
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Hi")
+        bot.send_message(message.from_user.id, "disable/enable/status")
     else:
         bot.send_message(message.from_user.id, 'write /help.')
 bot.polling(none_stop=True, interval=0)
-
-
+print (stat)
